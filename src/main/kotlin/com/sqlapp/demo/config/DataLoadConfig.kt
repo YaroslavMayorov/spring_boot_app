8e81034b186e99d1b89557186e1d469caa68e43e
bonnieblack14 <sasha.val2006@gmail.com>
2025-09-19 19:43:24 +0300

    Dataset loader class added

---
+++ src/main/kotlin/com/sqlapp/demo/config/DataLoadConfig.kt
@@ -0,0 +1,25 @@
+package com.sqlapp.demo.config
+
+
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Component
+
+
+@Component
+class DataLoadConfig(
+    private val jdbcTemplate: JdbcTemplate
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @PostConstruct
+    fun init() {
+        jdbcTemplate.execute(
+            """
+    CREATE TABLE IF NOT EXISTS passengers AS 
+    SELECT * FROM CSVREAD('classpath:data/titanic.csv');
+    """.trimIndent()
+        )
+    }
+}
